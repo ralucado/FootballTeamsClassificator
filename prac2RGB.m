921a3cfce4670c2bb4a1c3a7275779170d6b0c00
ralucado <ralucado@users.noreply.github.com>
2018-04-27 03:11:51 +0200

    mes comentaris

--- prac2RGB.m
+++ prac2RGB.m
@@ -32,14 +32,19 @@ falsPos = sum([madrid,acmilan,chelsea,juventus,liverpool,psv]>th2)/(6*37);
 disp('% falsos positius: ');
 disp(falsPos);
 
-%% Distribucio dels resultats
-c = categorical({'barcelona','madrid','milan', 'chelsea', 'juventus', 'liverpool', 'psv'});
+%% Etapa 4: Distribucio dels resultats
+%c = categorical({'barcelona','madrid','milan', 'chelsea', 'juventus', 'liverpool', 'psv'});
 %figure(), bar(c, [bcn;madrid;acmilan;chelsea;juventus;liverpool;psv], 'BarWidth', 1),  title('RGB 32 bins');
+%Fem un grafic per veure quin valor li ha sigut assignat a cada foto
+%Segons el valor de la barra sabem a quin equip pertany
+%1: bcn, 2:acmilan, ... TO DO: fer que aixo es vegi maco? nose com
 figure(), bar([bcn, acmilan], 'BarWidth', 1),  title('RGB 32 bins');
 
 
 %% Funcions auxiliars
+
 function arr=readImgs(team, th)
+%llegeix les imatges d'un equip i calcula l'equip al que pertany cadascuna
     arr = zeros(37:1);
     for k = 1:9
         name = sprintf(strcat(team,'/0%d.jpg'), k);
@@ -53,66 +58,94 @@ function arr=readImgs(team, th)
     end
 end
 
-function H=histograma(im,bins)
-        R = im(:,:,1);
-        G = im(:,:,2);
-        B = im(:,:,3);
-        H = [histcounts(R, bins, 'Normalization', 'probability'), ...
-             histcounts(G, bins, 'Normalization', 'probability'), ...
-             histcounts(B, bins, 'Normalization', 'probability')];
-end
-
-function EQ=equalize(im)
-        imh = rgb2hsv(im);
-        imh(:,:,3)=1;
-        EQ = hsv2rgb(imh);
-end
-
 function index=compareImg(im, w, h, threshold)
+%retorna l'index del equip que suposadament apareix mes a la imatge
     global bins; global models; global num_teams; global num_hists;
     [rows, cols, ~] = size(im);
     index = 0;
     maxCount = 0;
     step = 20;
+    %equalitzem la imatge a testejar
     im = equalize(im);
     %Per cada equip
     for k = 1 : num_teams
+        %agafem l'histograma model
         modelTeam = zeros(3, 96);
+        %total es el recompte de subimatges que s'han analitzat de la foto
+        %a testejar
         total = 0;
+        %count es el nombre de subimatges que tenen una presencia
+        %significativa del equip que s'esta avaluant
         count = 0;
         for i = 1 : step : rows-h
             for j = 1: step : cols-w
                 %Comparem un conjunt de sub-finestres amb els histogrames
                 %model de cada equip
+                %d es un acumulador per la distancia
+                %als histogrames model
                 d = 0;
                 total = total + 1;
+                %calculem l'histograma de la part de imatge que estem
+                %avaluant
+                part = im(i:i+w, j:j+h,:);
+                Hpart = histograma(part,bins);
                 for l = 1 : num_hists
                     modelTeam(1, :) = models(k, l, :);
-                    part = im(i:i+w, j:j+h,:);
-                    Hpart = histograma(part,bins);
+                    %en calculem la distancia i la sumem al acumulador
                     d = d + compareHists(Hpart, modelTeam(1, :));
                 end
+                %suposem que la distancia total entre l'histograma parcial
+                %de test i els models es la mitja aritmetica entre els 3
                 dT = d/3;
+                %si aquesta distancia mitjana supera cert treshold,
+                %determinem que la subimatge avaluada es prou semblant a
+                %les subimatges de mostra
                 if dT < threshold 
                     count = count + 1;
                 end
             end
         end
-        %Normalize count between 0 and 1 depending on the total samples taken
+        %Normalitzem el recompte fet en funcio del total de subimatges
+        %avaluades.
         count = count/total;
         if(count > maxCount)
+            %Guardem l'index de l'equip que ha tingut mes percentatge de
+            %coincidencies amb els histogrames de mostra
             maxCount = count;
             index = k;
         end
     end
 end
 
+function H=histograma(im,bins)
+%per a calcular l'histograma utilitzem la funcio HistCounts
+        R = im(:,:,1);
+        G = im(:,:,2);
+        B = im(:,:,3);
+        %Separem la imatge per canals R, G i B i guardem 32 valors per cada
+        %canal
+        H = [histcounts(R, bins, 'Normalization', 'probability'), ...
+             histcounts(G, bins, 'Normalization', 'probability'), ...
+             histcounts(B, bins, 'Normalization', 'probability')];
+end
+
+function EQ=equalize(im)
+%per equalitzar la imatge i ferla invariant a la iluminacio eliminem el
+%canal V de la imatge convertida a HSV i la tornem a passar a rgb
+        imh = rgb2hsv(im);
+        imh(:,:,3)=1;
+        EQ = hsv2rgb(imh);
+end
+
 function D=compareHists(H1, H2)
+%funcio per calcular la distancia entre 2 histogrames
     global bins;
     L = bins*3;
     Dreds = pdist2(H1(1:bins-1),H2(1:bins-1));
     Dgreens = pdist2(H1(bins:2*bins-1),H2(bins:2*bins-1));
     Dblues = pdist2(H1(2*bins:L),H2(2*bins:L));
+    %calculem la distancia entre 2 histogrames com la mitja aritmetica de
+    %la distancia pitagorica entre ells
     D = (Dblues+Dreds+Dgreens)/3;
 end
 
